8bb4e68c4e0fd6592911fcc1cee2f25dd99e75eb
Usk3210 <usk3210@gmail.com>
2023-02-14 00:05:19 +0530

    newcommit

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -6,7 +6,7 @@ const Homepage = () => {
 
   const [posts, setPosts] = useState([])
 
-
+  
   useEffect(() => {
     fetch("https://instacloneappbackend-deployment.onrender.com/allposts", {
       method: "get",
@@ -20,6 +20,34 @@ const Homepage = () => {
 
 
   }, [])
+
+  const submitComment =(e,postId)=>{
+    e.preventDefault() // avoid the page Refresh
+    const commentText = e.target.value
+    fetch("https://instacloneappbackend-deployment.onrender.com/comment",{
+      method:"put",
+      headers:{
+        "Content-Type": "application/json",
+        "Authorization": "bearer "+localStorage.getItem("token")
+
+      },
+      body: JSON.stringify({commentText:commentText,postId:postId})
+    }).then(res=>res.json())
+    .then((updatedPost)=>{
+      console.log(updatedPost)
+      const newPostArr = posts.map((oldpost)=>{
+        if(oldpost._id == updatedPost._id){
+          return updatedPost
+        }
+        else{
+          return oldpost
+        }
+      });
+      setPosts(newPostArr)
+    }).catch((err)=>{
+      console.log(err)
+    })
+  }
 
 
   return (
@@ -38,7 +66,10 @@ const Homepage = () => {
                 <i className="material-icons" style={{ color: "red" }}>favorite</i>
                 <h6>{post.title}</h6>
                 <p>{post.body}</p>
+                <form onSubmit={(e)=>{submitComment(e, post._id)}}>
                 <input type="text" placeholder='Enter comment' />
+                </form>
+            
               </div>
             </div>
           )
